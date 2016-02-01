f2f47e01454da5df9cdb98e246051462ad9cc2ec
Lucille Dhaleine <lucille.dhaleine@gmail.com>
2016-02-01 20:53:17 +0100

    [feat] app init

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+app.on('ready', createWindow);
+
+var mainWindow = null;
+
+function createWindow(){
+    mainWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        resizable: false,
+        title: 'Pomodoro Timer',
+        autoHideMenuBar: true,
+        backgroundColor: '#333'
+    });
+};
